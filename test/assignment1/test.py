ced5db7e29737f1c9c5c24dd95966e3112aab6e1
anjalidiggi <162709416+anjalidiggi@users.noreply.github.com>
2024-04-16 15:22:31 +0530

    pyspark assignment

--- test/assignment1/test.py
+++ test/assignment1/test.py
@@ -1,40 +1,66 @@
 import unittest
 from pyspark.sql import SparkSession
-from src.Assignment1.util import *
+from pyspark.sql.types import StructType, StructField, StringType
+from pyspark_assignment.src.assignment1.driver import only_iphone13  # Import your function from the module
+from pyspark_assignment.src.assignment1.driver import purchase_all_products, upgraded_customers
 
 
-class TestPurchaseAnalysis(unittest.TestCase):
-
+class MyTestCase(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        cls.spark = SparkSession.builder.appName("TestPurchaseAnalysis").getOrCreate()
+        cls.spark = SparkSession.builder \
+            .appName("Testing") \
+            .getOrCreate()
+
+
+        schema1 = StructType([
+            StructField("customer", StringType(), nullable=False),
+            StructField("product_model", StringType(), nullable=False)
+        ])
+        data1 = [
+            ("1", "iphone13"),
+            ("1", "dell i5 core"),
+            ("2", "iphone13"),
+            ("2", "dell i5 core"),
+            ("3", "iphone13"),
+            ("3", "dell i5 core"),
+            ("1", "dell i3 core"),
+            ("1", "hp i5 core"),
+            ("1", "iphone14"),
+            ("3", "iphone14"),
+            ("4", "iphone13")
+        ]
+        cls.purchase_data_df = cls.spark.createDataFrame(data=data1, schema=schema1)
 
     @classmethod
     def tearDownClass(cls):
         cls.spark.stop()
 
-    def test_only_iphone13_customers(self):
-        purchase_data_df = create_purchase_data_df(self.spark)
-        product_data_df = create_product_data_df(self.spark)
-        result = find_customers_with_only_iphone13(purchase_data_df, product_data_df)
-        expected_result = [(4,)]  # Change the format to match the actual result
-        result_list = [(row.customer,) for row in result.collect()]  # Convert Row objects to tuples
-        self.assertEqual(result_list, expected_result)
+    def test_only_iphone13(self):
+        result_df = only_iphone13()
 
-    def test_customers_upgraded_to_iphone14(self):
-        purchase_data_df = create_purchase_data_df(self.spark)
-        result_df = find_customers_upgraded_to_iphone14(purchase_data_df)
-        expected_result = [(1,), (3,)]  # Adjust the expected result based on your data
-        result_list = [(row.customer,) for row in result_df.collect()]
-        self.assertEqual(sorted(result_list), sorted(expected_result))
+        expected_result = [('4',)]
+        expected_schema = StructType([StructField("customer", StringType(), nullable=True)])
 
-    def test_customers_bought_all_products(self):
-        purchase_data_df = create_purchase_data_df(self.spark)
-        product_data_df = create_product_data_df(self.spark)
-        result = find_customers_bought_all_products(purchase_data_df, product_data_df)
-        expected_result = [(1,)]
-        self.assertEqual(result.collect(), expected_result)
+        expected_df = self.spark.createDataFrame(data=expected_result, schema=expected_schema)
+
+        self.assertEqual(result_df.collect(), expected_df.collect())
+
+    def test_purchase_all_products(self):
+        result_df = purchase_all_products()
+
+        expected_result = [('1',)]
+        expected_schema = StructType([StructField("customer", StringType(), nullable=False)])
+
+        self.assertEqual(result_df.collect(), expected_result)
+        self.assertEqual(result_df.schema, expected_schema)
+
+    def test_upgraded_customers(self):
+        result = upgraded_customers()
+        expected_result = ['1', '3',] # Assuming customer IDs '1' and '3' have upgraded their devices
+        result_list = [row.customer for row in result.collect()]
+        self.assertCountEqual(result_list, expected_result)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()
